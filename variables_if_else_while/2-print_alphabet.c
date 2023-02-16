a2c117294d80cb0dea6e2a7682c7759f8ee39140
KrasniKot <6187@holbertonstudents.com>
2023-02-16 04:53:55 -0800

    m

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	int nl = 8;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar (c);
+		putchar (nl);
+	}
+
+	return (0);
+}
