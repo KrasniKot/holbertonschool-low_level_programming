dd006789908d52bc0f973e25b811d68a883fa87e
KrasniKot <6187@holbertonstudents.com>
2023-02-15 17:06:56 -0800

    m

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Print the last digit of n > 0, < 0, = 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else
+	{
+		if (n < 0)
+		{
+			printf("is negative\n");
+		}
+		else
+		{
+			printf("is zero\n");
+		}
+	}
+	return (0);
+}
