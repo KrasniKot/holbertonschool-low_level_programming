aee51f096f9621ef5829282d23bc03cd22110d8a
KrasniKot <6187@holbertonstudents.com>
2023-02-16 07:10:30 -0800

    m

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,11 +10,12 @@ int main(void)
 	int c;
         int nl = 10;
 
-	for (c = 48; c < 103; c++)
-	{
-		if (c < 58 && c > 96)
-			putchar (c);
-	}
+	for (c = 48; c < 58; c++)
+		putchar (c)
+
+	for (c = 97; c < 103; c++)
+		putchar (c)
+
 
 	putchar (nl);
 
