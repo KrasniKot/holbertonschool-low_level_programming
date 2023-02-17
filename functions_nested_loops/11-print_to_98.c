b1543cbdd91634cd64c8d5d233399c405b29b8bf
KrasniKot <6187@holbertonstudents.com>
2023-02-17 11:52:57 -0800

    m

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints from given number to 98.
+ * @n: print_to_98 variable.
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+		for (n ; n == 98; n++)
+		{
+			printf("%i, ", n);
+		}
+	}
+}
