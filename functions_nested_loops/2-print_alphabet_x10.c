d49c97ac2ec2aa0a62ce5173787853dba218a241
KrasniKot <6187@holbertonstudents.com>
2023-02-16 17:19:14 -0800

    m

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet.
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i > 11; i++)
+		for (x = 97; x < 123; x++)
+			_putchar(x)
+
+	_putchar(10)
+}
