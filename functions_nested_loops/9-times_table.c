c1865f82dfc48da6ee829d3e61fe6db915918b74
KrasniKot <6187@holbertonstudents.com>
2023-02-17 09:51:09 -0800

    m

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int x, y; prod;
+	    
+	    for (y = 0; y <= 9; y++)
+	    
+		    for (x = 0; x <= 9; x++)
+		    {
+			    prod = x * y;
+			    if (x == 0)
+			    {
+				    _putchar('0')
+			    }
+			    else if
+			    {
+				    if (prod <=9)
+				    {
+					    _putchar(',');
+					    _putchar(' ');
+					    _putchar(prod + '0');
+				    }
+				    else
+				    {
+					    _putchar(',');
+                                            _putchar(' ');
+					    _putchar(prod / 10 + '0');
+					    _putchar(prod % 10 + '0');
+				    }
+			    }
+		    _putchar(10);
+		    }
+}
