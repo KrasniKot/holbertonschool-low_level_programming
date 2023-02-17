075d93dd04974359b151f31e8b61a3b378fa79b8
KrasniKot <6187@holbertonstudents.com>
2023-02-17 03:43:19 -0800

    m

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * _islower - checks if "c" is lowercase.
+ *
+ * Return: 1 if lowercase, 0 if uppercase.
+ */
+int _islower(int c)
+{
+	if (c > 97 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
