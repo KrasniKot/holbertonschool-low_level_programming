2b0049f0c398b1226185505f268889adec4731e2
KrasniKot <6187@holbertonstudents.com>
2023-02-17 03:59:02 -0800

    m

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if c is alpha.
+ *@c: _isalpha variable.
+ *Returns: 1 if alpha, 0 if nonalpha.
+ */
+int _isalpha(int c)
+{
+	if (c > 64 && c < 91 || c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
