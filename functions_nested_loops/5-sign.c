bb42a1a9718704fed5423f33e00e3e04cdf52739
KrasniKot <6187@holbertonstudents.com>
2023-02-17 04:25:51 -0800

    m

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - evaluates sign of n.
+ * @n: print_sing variable.
+ * Return: +, 1 if possitive, -, -1 if negative, 0 if zero.
+ */
+int print_sign(int n);
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar (43);
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar (95);
+		return (-1);
+	}
+	else
+	{
+		_putchar (48);
+		return (0);
+	}
+}
