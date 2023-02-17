e4882f1f645f3336a2a5f3260cd7fe1d1e480d8a
KrasniKot <6187@holbertonstudents.com>
2023-02-17 05:07:38 -0800

    m

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value of a.
+ * @a: _abs variable.
+ * Return: absolute value of a.
+ */
+int _abs(int a)
+{
+	int a;
+	int c = 2;
+
+	return ((a * c) / c);
+}
