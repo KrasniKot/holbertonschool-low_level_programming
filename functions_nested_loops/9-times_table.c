a74d1c6b390fe94a57d108a2fa1a158d366e36c8
KrasniKot <6187@holbertonstudents.com>
2023-02-17 10:03:45 -0800

    m

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,33 +8,34 @@
 void times_table(void)
 {
 	int x, y;
-	    
-	    for (y = 0; y <= 9; y++)
-	    {
-		    for (x = 0; x <= 9; x++)
-		    {
-			    int prod = x * y;
 
-			    if (x == 0)
-			    {
-				    _putchar('0');
-			    }
-			    else if (prod <=9)
-			    {
-					    _putchar(',');
-					    _putchar(' ');
-					    _putchar(' ');
-					    _putchar(prod + '0');
-				    }
-				    else
-				    {
-					    _putchar(',');
-                                            _putchar(' ');
-					    _putchar(prod / 10 + '0');
-					    _putchar(prod % 10 + '0');
-				    }
-			    }
-		    _putchar(10);
-	    }
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			int prod = x * y;
+
+			if (x == 0)
+
+			{
+				_putchar('0');
+			}
+			else if (prod <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+		}
+		_putchar(10);
+	}
 
 }
