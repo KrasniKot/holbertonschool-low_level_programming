08fe94d9e706411d275d317e1c3933c4c57a0c9d
KrasniKot <6187@holbertonstudents.com>
2023-02-19 11:38:58 -0800

    m

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i > 100; i++)
+	for (i = 0; i < 9; i++)
 	{
-		putchar (i / 10 + '0');
-		putchar (i % 10 + '0');
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar (i + '0');
+			putchar (j + '0');
+			if (i == 8 && j == 9)
+			{
+				break;
+			}
+			putchar (',');
+			putchar (' ');
+		}
 	}
-	
+	putchar (10);
+
 	return (0);
 }
