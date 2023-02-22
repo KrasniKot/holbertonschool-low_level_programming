cb211d0e1eda43a11e5141ca15ce1efd8693cb33
KrasniKot <6187@holbertonstudents.com>
2023-02-22 08:52:02 -0800

    m

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 14 times from 0 to 9.
+ *
+ * Return: 0.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; for j < 14; j++)
+	{
+		for (i = 48; i < 59; i++)
+		{
+			_putchar (i);
+		}
+	}
+}
