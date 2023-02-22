4acb534cb8f83cce2d5e017ea95cf6c36266e668
KrasniKot <6187@holbertonstudents.com>
2023-02-22 09:25:13 -0800

    m

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,23 @@
  */
 int more_numbers(void)
 {
-	int i, j;
+	int i, j, n;
+	int n = 10;
 
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i < 10)
-			{
-				_putchar (i + '0');
-			}
-			else
+			if (i > 9)
 			{
 				_putchar (i / 10 + '0');
 				_putchar (i % 10 + '0');
 			}
+			else 
+			{
+				_putchar (i + '0');
+			}
 		}
-		return (j);
+		return (n);
 	}
 }
