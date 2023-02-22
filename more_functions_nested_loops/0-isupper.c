b115390ca80daa7e0c3a6b863c194806e88d6895
KrasniKot <6187@holbertonstudents.com>
2023-02-22 07:41:56 -0800

    m

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * _isupper - checks for uppecase characters.
+ * @c: function variable.
+ * Return: 1 if uppercase, 0 if not.
+ */
 int _isupper(int c)
 {
 	if (c > 64 && c < 91)
