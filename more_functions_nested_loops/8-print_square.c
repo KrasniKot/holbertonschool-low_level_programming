0f4a1a65f51446d3443adaef9de5f4fbbca2fb6a
KrasniKot <6187@holbertonstudents.com>
2023-02-23 05:21:23 -0800

    m

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square.
+ * @size: function variable.
+ * Return: 0.
+ */
+void print_square(int size)
+{
+	int y, x;
+
+	if (size <= 0)
+		_putchar;
+	else
+	{
+		for (y = 0; y < size; y++)
+		{
+			for (x = 0; x < size; x++)
+			{
+				_putchar ('#');
+			}
+			_putchar (10);
+		}
+	}
+}
