f3673d0bc0fdc848b51a857a7e93dd52c74f1dca
KrasniKot <6187@holbertonstudents.com>
2023-02-23 05:56:08 -0800

    m

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints int, Fizz, Buzz or FizzBuzz.
+ *
+ * Return; 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Fizz ");
+			}
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
