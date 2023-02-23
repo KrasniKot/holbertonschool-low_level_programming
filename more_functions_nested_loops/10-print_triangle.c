ca44378c9877eacc4bfa23404684fd712542887c
KrasniKot <6187@holbertonstudents.com>
2023-02-23 08:20:00 -0800

    m

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle.
+ * @size: function variable.
+ * Return: 0.
+ */
+void print_triangle(int size)
+{
+	int y, x, p;
+	int s = 1;
+	int t = size;
+
+	if (size <= 0)
+		_putchar (10);
+	else
+	{
+		for (y = 0; y < size; y++)
+		{
+			for (p = 1; p < t; p++)
+			{
+				putchar (' ');
+			}
+
+			for (x = 1; x < s; x++)
+			{
+				_putchar (#);
+			}
+			_putchar (10);
+			s++;
+			t++;
+		}
+	}
+}
