1e642c821654bb1ef7b245780793736b7f691082
KrasniKot <6187@holbertonstudents.com>
2023-02-27 08:19:27 -0800

    m

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reverse string.
+ * @s: function variable.
+ */
+void print_rev(char *s)
+{
+	int k = 0;
+
+	while (s[k])
+	{
+		k++;
+	}
+
+	k -= 1;
+
+	while (k >= 0)
+	{
+		_putchar (s[k]);
+		k--;
+	}
+
+	_putchar (10);
+}
