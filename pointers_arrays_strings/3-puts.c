7f5408f44fba880005b0f91c033306d6cd7b70b9
KrasniKot <6187@holbertonstudents.com>
2023-02-27 07:32:04 -0800

    m

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,17 @@
 #include "main.h"
 
+/**
+ * _puts - prints a strin.
+ * @str: function variable.
+ */
 void _puts(char *str)
 {
+	int i = 0;
+
 	while (*str != '\0')
 	{
-		_putchar (*(str++));
+		_putchar (str[i]);
+		i++;
 	}
+	_putchar (10);
 }
