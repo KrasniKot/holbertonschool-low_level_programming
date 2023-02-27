beec0f121bb1bb7cf4c99fa881fbd151634091d0
KrasniKot <6187@holbertonstudents.com>
2023-02-27 06:23:50 -0800

    m

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of a and b.
+ * @a: function variable.
+ * @b: function variable.
+ *
+ * swap_int - swaps the value of a and b.
+ * @a: function variable.
+ * @b: function variable.
+ */
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+
+	*a = *b;
+	*b = n;
+}
