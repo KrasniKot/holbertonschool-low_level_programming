ce485102b44a17a1abfe58c958215c1ff7d702d6
KrasniKot <6187@holbertonstudents.com>
2023-02-28 09:52:37 -0800

    m

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,12 +4,16 @@
  * _strcmp - compares to strings.
  * @s1: string 1
  * @s2: string 2.
- * Return: 0 if string 1 and 2 are the same, 1 if not.
+ * Return: 0 if string 1 and 2 are the same, 1 if 1 is greater than 2, -1 if not.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int k = 0, h = 0;
+	int n = 0, k = 0, h = 0;
 
+	while(s2[n])
+	{
+		n++;
+	}
 	while (s1[k])
 	{
 		if (s1[k] != s2[k])
@@ -19,9 +23,13 @@ int _strcmp(char *s1, char *s2)
 		}
 		k++;
 	}
-	if (h != 0)
+	if (h != 0 && k > n)
 	{
 		return (1);
+	}
+	else if (h != 0 && k < n)
+	{
+		return (-1);
 	}
 	else
 	{
