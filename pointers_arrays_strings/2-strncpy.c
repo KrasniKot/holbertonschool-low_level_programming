60b4d8dc16a3279bd49a4a46eac4383867eb8c8d
KrasniKot <6187@holbertonstudents.com>
2023-02-28 08:54:44 -0800

    m

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @dest: destination.
  * @src: source.
  * @n: limit.
+ * Return: dest.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -18,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i++] = '\0';
 	}
-	
+
 	return (dest);
 }
 
