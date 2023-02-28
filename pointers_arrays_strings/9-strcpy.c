ab52be50ff587017b04404a4fd3b577496d96243
KrasniKot <6187@holbertonstudents.com>
2023-02-28 06:05:09 -0800

    m

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies src's content into dest.
+ * @dest: destination.
+ * @src: source.
+ * Return: *dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, k = 0;
+
+	while (src[k])
+	{
+		k++
+	}
+
+	for (i = 0; i <= k; i++)
+	{
+		*dest[i] = *src[i];
+	}
+	return (*dest);
+}
