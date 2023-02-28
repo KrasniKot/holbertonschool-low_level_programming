2e003572a0fc348e4f1c6c31726c7c337f82a0e3
KrasniKot <6187@holbertonstudents.com>
2023-02-28 04:09:47 -0800

    m

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ * @str: function variable.
+ */
+
+void puts2(char *str)
+{
+	int i = 0, k = 0;
+
+	while (str[k])
+	{
+		k++
+	}
+
+	for (i = 0; i < k - 1; i++)
+	{
+		_putchar (k[i]);
+		i++;
+	}
+	_putchar (10);
+}
+
