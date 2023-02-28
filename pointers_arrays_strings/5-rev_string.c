6b13b08e2ad3659fb9474f3c568af70666d193cf
KrasniKot <6187@holbertonstudents.com>
2023-02-28 03:55:06 -0800

    m

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,20 +6,20 @@
  */
 void rev_string(char *s)
 {
-	int k, i;
+	int k, i = 0;
 	char l;
 
 	while (s[k])
 	{
-		k++
+		k++;
 	}
 
-	k -= 1
+	k -= 1;
 
-	for (i = 0; i < k / 2; i++)
+	for (i = k; i >= k / 2; i--)
 	{
-		l = s[k - i];
-		s[k - i] = s[i];
+		l = s[i];
+		s[i] = s[k - i];
 		s[k - i] = l;
 	}
 	_putchar (10);
