385effb44d1759fcf6454ced84d53362720e8fb2
KrasniKot <6187@holbertonstudents.com>
2023-03-01 05:34:13 -0800

    m

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,20 @@
-#include "main.h"
+#include <stdio.h>
 
 int _atoi(char *s)
 {
-	int n = 1, i = 0;
+        int n = 0;
 
-	while (*s++)
-	{
-		if (*s == '-');
-		{
-			n *= -1;
-
-		}
+        while (*s)
+        {
+                if (*s >= '0' && *s <= '9')
+                {
+                       n = (n * 10) + (*s - '0');
+                }
+                else if (*s == '-')
+{
+n =- 1;
+}
+s++;
+}
+return (n);
+}
