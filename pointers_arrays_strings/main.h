0d876520b9e47f00e729c5089da020ff04a88a25
KrasniKot <6187@holbertonstudents.com>
2023-03-02 09:02:07 -0800

    m

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -18,3 +18,4 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
