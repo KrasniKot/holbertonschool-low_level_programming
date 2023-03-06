40485bb879b0b7342e7279bd168f11c4998ca3f8
KrasniKot <6187@holbertonstudents.com>
2023-03-06 05:18:26 -0800

    m

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print-rev_recursion - prints a string in reverse.
+ * @s: array.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*!s)
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar (*s);
+}
