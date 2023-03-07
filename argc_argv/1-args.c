df4e27b781cd1d230c8902f6f0f6d565c316f781
KrasniKot <6187@holbertonstudents.com>
2023-03-07 06:51:40 -0800

    m

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints @argc.
+ * @argc: amount of given arguments.
+ * @argv: array.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
