184b8359cac0dc753b6bd800470624d2458d0bae
KrasniKot <6187@holbertonstudents.com>
2023-03-07 09:10:17 -0800

    m

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,28 +13,23 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 	{
-		putchar (48);
-		putchar (10);
-	}
-	else
-	{
-		for (j = 0; argv[j]; j++)
-		{
-			for (k = 0; argv[j][k]; k++)
-			{
-				if (argv[j][k] < 48 || argv[j][k] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-		}
-
-		for (i = 0; i < argc; i++)
-		{
-			a += atoi(argv[i]);
-		}
-		printf("%i\n", a);
 		return (0);
 	}
+	for (j = 0; argv[j]; j++)
+	{
+		for (k = 0; argv[j][k]; k++)
+		{
+			if (argv[j][k] < 48 || argv[j][k] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (i = 0; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%i\n", a);
+	return (0);
 }
