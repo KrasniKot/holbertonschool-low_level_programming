74de4e8b12778aef23848377c4ee550d87f26501
KrasniKot <6187@holbertonstudents.com>
2023-03-07 06:00:07 -0800

    m

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,9 +3,13 @@
 /**
  * main - prints its name.
  * @argc: args counter.
- * @argv: array.
+ * @argv: array
+ * Return: 0.
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
+
+	return (0);
 }
