fa7bc96209462c8b7f26be1cf3d4b013e36d93eb
KrasniKot <6187@holbertonstudents.com>
2023-03-13 06:46:08 -0700

    m

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of chars, and initializes it.
+ * @size: amount of bytes.
+ * @c: byte.
+ * Return: @p.
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *P;
+
+	char *p = (char *)malloc(size);
+
+	p[0] = c;
+
+	if (!size)
+		return ('\0');
+
+	p = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (p);
+}
---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int comparing(char *s1, char *s2, int i)
+{
+	if (s1[i] != s2[i])
+	{
+		if (s1[i] == '*' || s2[i] == '*')
+		{
+		}
+		return (0)
+	}
+	else if (
+}
+int wildcmp(char *s1, char *s2)
+{
+	return (comparing(s1, s2, 0));
+}
