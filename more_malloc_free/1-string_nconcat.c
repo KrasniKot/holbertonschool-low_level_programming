fe2f0479faecef90f72dac11698b3bf1925c74fe
KrasniKot <6187@holbertonstudents.com>
2023-03-15 09:51:46 -0700

    m

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[h] && h <= n)
 		h++;
 
-	c = malloc(sizeof(char) * (k + h + 1));
+	c = malloc(sizeof(char) * (k + h));
 
 	if (!c)
 	{
