e29276900bfdc726a67a3d908094fdcd7e5567c7
KrasniKot <6187@holbertonstudents.com>
2023-03-20 11:37:24 -0700

    m

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,18 +13,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *nname;
 	char *nowner;
 	int i, k;
+	
+	if (!name || !owner)
+		return (NULL);
 
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
+
+	if (!d)
+		return (NULL)
+
 	nname = malloc(sizeof(name));
+
+	if (!nname)
+		return (NULL);
 
 	for (i = sizeof(name); i >= 0; i--)
 		nname[i] = name[i];
 
 	nowner = malloc(sizeof(owner));
+	if (!nowner)
+		return (NULL);
 
-	for (k = sizeof(owner); k <= 0; k++)
+	for (k = sizeof(owner); k >= 0; k--)
 		nowner[k] = owner[k];
 
 	d->name = nname;
