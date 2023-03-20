2a6775a4fa32b992d477d9a7ae67d73bf55a86bc
KrasniKot <6187@holbertonstudents.com>
2023-03-20 12:31:41 -0700

    m

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -28,7 +28,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (k = sizeof(owner); k >= 0; k--)
 		nowner[k] = owner[k];
 	d->name = nname;
+	if (!d->name)
+	{
+		free(d);
+		return (NULL);
+	}
 	d->age = age;
 	d->owner = nowner;
+	if (!d->owner)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 	return (d);
 }
