413b05693d81ba1d5a7b9d9d2744d9f5bfab833e
KrasniKot <6187@holbertonstudents.com>
2023-03-20 12:03:26 -0700

    m

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,42 +6,35 @@
  * @name: name.
  * @age: age.
  * @owner: owner.
- * Return: a pointer to dog_t-
+ * Return: a pointer to dog_t.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *nname, *nowner;
 	int i, k;
 
-	if (!name || !owner)
-		return (NULL);
-
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
 
-	if (!d)
-		return (NULL);
-
 	nname = malloc(sizeof(name));
-
-	if (!nname)
-		return (NULL);
 
 	for (i = sizeof(name); i >= 0; i--)
 		nname[i] = name[i];
 
 	nowner = malloc(sizeof(owner));
 
-	if (!nowner)
-		return (NULL);
-
 	for (k = sizeof(owner); k >= 0; k--)
 		nowner[k] = owner[k];
 
 	d->name = nname;
+	if (!d->name)
+		return (free(d), NULL);
+
 	d->age = age;
 	d->owner = nowner;
+	if (!d->owner)
+		return (free(d->name), free(d), NULL);
 
 	return (d);
 }
--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * main - check the code
