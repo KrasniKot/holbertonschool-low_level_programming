c6d9a2d82d8076bf6f0ac7cd88bf089849a30a3f
KrasniKot <6187@holbertonstudents.com>
2023-03-22 07:03:55 -0700

    checking3/2

---
+++ function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - calls function get_op_func.
+ * @argc: number of arguments given.
+ * @argv: array of arguments given.
+ * Return: 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (!get_op_func(argv[2]));
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds.
+ * @a: x.
+ * @b: y.
+ * Return: @a + @b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns @a - @b.
+ * @a: x.
+ * @b: y.
+ * Return: @a - @b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplicates.
+ * @a: x.
+ * @b: y.
+ * Return: @a * @b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides.
+ * @a: x.
+ * @b: y.
+ * Return: @a / @b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of a division.
+ * @a: x.
+ * @b: y.
+ * Return: @a % @b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
