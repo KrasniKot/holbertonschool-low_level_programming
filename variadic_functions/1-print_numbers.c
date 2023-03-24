dde1f8356b78ee5f24f56e540a25de27bf411595
KrasniKot <6187@holbertonstudents.com>
2023-03-24 04:36:40 -0700

    ifcorrected

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,5 +1,10 @@
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - prints the numbers given as arguments.
+ * @separator: separator.
+ * @n: number of arguments.
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
@@ -10,7 +15,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(l, int));
-		printf("%s", separator);
+		if (i < n - 1 && separator)
+			printf("%s", separator);
 	}
 	putchar (10);
 	va_end(l);
