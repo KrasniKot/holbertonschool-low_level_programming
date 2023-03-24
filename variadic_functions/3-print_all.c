aa2a0d098b44841857f7f51776d846e675fbf1a4
KrasniKot <6187@holbertonstudents.com>
2023-03-24 10:20:38 -0700

    Separator_Added_and_Float

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,31 +1,53 @@
 #include "variadic_functions.h"
 
-void print_chr(va_list(l))
+/**
+ * print_chr - prints a char.
+ * @l: char.
+ */
+void print_chr(va_list l)
 {
 	printf("%c", va_arg(l, int));
 }
-void print_int(va_list(l))
+/**
+ * print_int - prints an int.
+ * @l: int.
+ */
+void print_int(va_list l)
 {
 	printf("%d", va_arg(l, int));
 }
-void print_flt(va_list(l))
+/**
+ * print_flt - prints a float.
+ * @l: float.
+ */
+void print_flt(va_list l)
 {
-	printf("%f", va_arg(l, double));
+	printf("%.f", va_arg(l, double));
 }
-void print_str(va_list(l))
+
+/**
+ * print_str - prints a string.
+ * @l: string.
+ */
+void print_str(va_list l)
 {
 	printf("%s", va_arg(l, char *));
 }
 
+/**
+ * print_all - call the concerned function.
+ * @format:  list of types of arguments.
+ */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	va_list l;
+	int i = 0, j = 0;
 
-	va_start (l, format);
+	va_list (l);
+	char *separator = "";
 
-	typp pfs[] =
-	{
+	va_start(l, format);
+
+	typp pfs[] = {
 		{"c", print_chr},
 		{"i", print_int},
 		{"f", print_flt},
@@ -33,14 +55,22 @@ void print_all(const char * const format, ...)
 		{NULL, NULL}
 	};
 
-	while (pfs[i].a)
+	while (format[i] && format)
 	{
-		if(*pfs[i].a = format[i])
+		while (pfs[j].a)
 		{
-			pfs[i].fs(l);
+			if (*pfs[j].a == format[i])
+			{
+				printf("%s", separator);
+				pfs[j].fs(l);
+				separator = ", ";
+			}
+			j++;
 		}
+		j = 0;
 		i++;
 	}
 	putchar (10);
+	va_end(l);
 
 }
