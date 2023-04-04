e12dbdb17b5a51237b4b668c4c5969da51c866df
KrasniKot <6187@holbertonstudents.com>
2023-04-04 07:48:00 -0700

    4finished

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list.
+ * @head: pointer to list.
+ */
+void free_list(list_t *head)
+{
+	while (head)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
