f431072cf77b46ac6ff3871578e5e8dd7b59c309
KrasniKot <6187@holbertonstudents.com>
2023-04-07 17:04:42 -0700

    4 corrected(???)=

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -8,5 +8,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	return (set_bit(n, index));
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+	return (1);
 }
