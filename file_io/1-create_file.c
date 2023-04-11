d6043fbaa1f6f6175ec239152db0bfcfbbaa3983
KrasniKot <6187@holbertonstudents.com>
2023-04-11 11:39:04 -0700

    1 finished

---
+++ file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: file name.
+ * @text_content: text content.
+ * Return: -1 if it fails, 1 otherwise.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	int wfile, i = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+
+	while (text_content[i])
+		i++;
+
+	wfile = write(file, text_content, i);
+
+	if (!wfile)
+		return (-1);
+
+	close(file);
+	return (1);
+}
