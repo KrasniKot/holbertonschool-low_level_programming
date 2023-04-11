f64e41aaaad4c9ed54d59d098be3f084a4479596
KrasniKot <6187@holbertonstudents.com>
2023-04-11 12:45:16 -0700

    changed oflag (write only -> rw)

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	int file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	int wfile, i = 0;
 
 	if (!filename)
