4665d6d15d77c3fb78029122c28f9437f8fd511c
KrasniKot <6187@holbertonstudents.com>
2023-04-11 13:06:52 -0700

    2 Finished

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file.
+ * @filename: filename.
+ * @text_content: text content.
+ * Return: -1 if it fails, 1 otherwise.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file = open(filename, O_WRONLY | O_APPEND);
+	int i = 0, wfile;
+
+	if (!filename || file == -1)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+
+	wfile = write(file, text_content, i);
+
+	if (wfile == -1)
+		return (-1);
+
+	close(file);
+	return (1);
+}
