0450a8307c8a36076f1a2876d2b12457f1482474
KrasniKot <6187@holbertonstudents.com>
2023-04-13 14:46:35 -0700

    4 Finished

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * *hash_table_get - gets the value of @key.
+ * @ht: hash table.
+ * @key: key.
+ * Return: NULL if it fails, value of key otherwise.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int i = key_index(key, ht->size);
+
+	while (ht && key && ht->array[i])
+	{
+		if (!strcmp(ht->array[i]->key, key))
+		{
+			return (ht->array[i]->value);
+		}
+		i++;
+	}
+	return (NULL);
+}
